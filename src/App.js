e2df292e7bcf47399dc687fd6c36ca6d5f767864
Meroni <114459394+Meroni-bcg@users.noreply.github.com>
2023-03-08 23:59:12 -0800

    added key prop to each child component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
 import './App.css';
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 // Make sure to import the component we just built:
-import ColorBlock from './ColorBlock'
+import ColorBlock from './ColorBlock';
+import ColorForm from './ColorForm';
 
 function App(){
-    let colors = [
-        'violet', 'blue',
-        'lightblue', 'green',
-        'greenyellow', 'yellow',
-        'orange', 'red'
-    ]
+  let colors = [
+      'violet', 'blue',
+      'lightblue', 'green',
+      'greenyellow', 'yellow',
+      'orange', 'red'
+  ];
     
     let colorMap = colors.map((color, i) => {
         return (
-            <ColorBlock color={color} />
-        )
-    })
+            <ColorBlock key={i} color={color} />
+        );
+    });
+
+   
 
     return (
         <div className="App">
             {colorMap}
+           
         </div>
-    )
+    );
 }
 
 
+
 export default App;
+
+
